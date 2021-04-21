fea4e4fd692f001fa814754c6c4c1bebed95e210
Cinga <czani89@gmail.com>
2021-04-21 10:52:52 +0200

    added greeting span

--- greet.js
+++ greet.js
@@ -4,6 +4,7 @@ let countRad = document.querySelector(".counter");
 let button = document.querySelector(".btn");
 let greetSpan = document.querySelector(".greet");
 
+var counter = 0;
 button.addEventListener("click", function () {
   let selectLanguage = greetRad.querySelector("input[name='greeting']:checked")
     .value;
@@ -12,12 +13,19 @@ button.addEventListener("click", function () {
   if (selectLanguage) {
     if (selectLanguage === "english") {
       greetSpan.value = "Good morning, " + nameText.value;
+      counter++;
     } else if (selectLanguage === "afrikaans") {
       greetSpan.value = "Goie more, " + nameText.value;
+      counter++;
     } else if (selectLanguage === "xhosa") {
       greetSpan.value = "Molo, " + nameText.value;
+      counter++;
     }
+  } else {
+    greetSpan.value = "";
   }
 
   greetSpan.innerHTML = greetSpan.value;
+
+  countRad.innerHTML = counter;
 });
