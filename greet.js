9eaa3aaec3a9aa6e58ccedb4519018e84808811f
czani89 <czani89@gmail.com>
2021-05-04 11:44:49 +0200

    added span for alert messages

--- greet.js
+++ greet.js
@@ -4,10 +4,10 @@ let countRad = document.querySelector(".counter");
 let button = document.querySelector(".greetMe");
 let resetButton = document.querySelector(".btnReset");
 let greetSpan = document.querySelector(".greet");
+let alertMessage = document.querySelector(".message")
 
 
-// var counter = 0;
-var parsedNames = {};
+// var counter = 0
 
 //retrive names from localstorage
 
@@ -22,41 +22,23 @@ button.addEventListener("click", function () {
 
   var radGroup = langDiv.getElementsByTagName("input")
 
-  console.log(radGroup);
-
   console.log(nameText.value);
   console.log(typeof (nameText.value));
 
-  if (nameText.value === "") {
-    alert("Please enter your name")
+  if (nameText.value === "" && !greetRad.checked) {
+    alertMessage.innerHTML = "Please enter your name and select language!"
+  } else if (nameText.value === "") {
+    alertMessage.innerHTML = "Please enter your name!"
+  } else if (!greetRad.checked) {
+    alertMessage.innerHTML = "Please select language!"
   }
   else {
     greetMe.setName(lowerName.toLowerCase());
+    let selectLanguage = document.querySelector("input[name='greeting']:checked").value;
+    greetMe.languages(selectLanguage, greetMe.getName());
   }
-
-  var flagChecked = false;
-
-  for (var i = 0; i < radGroup.length; i++) {
-    if (radGroup[i].checked) {
-      let selectLanguage = document.querySelector("input[name='greeting']:checked").value;
-      greetMe.languages(selectLanguage, greetMe.getName());
-      flagChecked = true;
-    }
-  }
-
-  if (!flagChecked) {
-    alert("Please select a language")
-  }
-
-  console.log(greetMe.newM());
 
   localStorage.setItem("names", JSON.stringify(greetMe.newM()))
-
-  if (localStorage["names"]) {
-    parsedNames = JSON.parse(localStorage.getItem("names"))
-  }
-
-  console.log(parsedNames);
 
   countRad.innerHTML = greetMe.counter()
 
@@ -64,6 +46,10 @@ button.addEventListener("click", function () {
 
   nameText.value = "";
 });
+
+if (localStorage["names"]) {
+  nameMap = JSON.parse(localStorage.getItem("names"))
+}
 
 // if (localStorage.getItem("names") === undefined) {
 //   parsedNames = [];
